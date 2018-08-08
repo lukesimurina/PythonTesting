ce2441b6698665d45f9be7c32f053789a5c4060c
luke <luke@simurina.com>
2018-08-08 22:48:38 +1000

    a

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 print ("""Welcome to this program with finds the area of 2 different kinds of shapes. The two shapes are circles and triangles.""")
 print ("Calculator is starting up")
 
-option = raw_input("Enter C for Circle or T for Triangle: ")
+option = input("Enter C for Circle or T for Triangle: ")
 
 if option == 'C' or option == 'c':
-  radius = float(raw_input("Enter Radius: "))
+  radius = float(input("Enter Radius: "))
   area = 3.14159 * radius**2
   print ("Area is: " + str(area))
 elif option == 'T' or option == 't':
-  base = float(raw_input("Enter Base: "))
-  height = float(raw_input("Enter Height: "))
+  base = float(input("Enter Base: "))
+  height = float(input("Enter Height: "))
   area = 0.5 * base * height
   print ("Area is: " + str(area))
 else:
